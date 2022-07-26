6ef29a2c3ade5f999072b31eea72d17da3f5dc43
manogupt <manojgupta123929@gmail.com>
2022-07-26 22:10:08 +0530

    Added : to retrive all entries from DB

---
+++ src/com/bridgelabz/AddressBook.java
@@ -0,0 +1,59 @@
+package com.bridgelabz;
+
+import java.sql.*;
+import java.util.*;
+
+public class AddressBook 
+{
+	Connection connection;
+
+    private Connection getConnection() {
+
+        String URL_JD = "jdbc:mysql://localhost:3306/payrollService?user=root";
+        String USER_NAME = "root";
+        String PASSWORD = "Arun@12345";
+
+        Connection connection = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            System.out.println("Drivers loaded!!");
+            connection = DriverManager.getConnection(URL_JD,USER_NAME,PASSWORD);
+            System.out.println("connection Established!!");
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+
+
+    public List<Contacts> retrieveData() {
+        ResultSet resultSet = null;
+        List<Contacts> addressBookList = new ArrayList<Contacts>();
+        try (Connection connection = getConnection()) {
+            Statement statement = connection.createStatement();
+            String sql = "select * from AddressBook";
+            resultSet = statement.executeQuery(sql);
+            int count = 0;
+            while (resultSet.next()) {
+                Contacts contactInfo = new Contacts();
+                contactInfo.setFirstName(resultSet.getString("firstName"));
+                contactInfo.setLastName(resultSet.getString("Lastname"));
+                contactInfo.setAddress(resultSet.getString("address"));
+                contactInfo.setCity(resultSet.getString("city"));
+                contactInfo.setState(resultSet.getString("state"));
+                contactInfo.setZip(resultSet.getInt("zip"));
+                contactInfo.setPhoneNumber(resultSet.getString("phoneNumber"));
+                contactInfo.setEmailId(resultSet.getString("email"));
+                contactInfo.setBookName(resultSet.getString("bookName"));
+                contactInfo.setContactType(resultSet.getString("contactType"));
+                contactInfo.setDateAdded(resultSet.getDate("Date_added").toLocalDate());
+
+                addressBookList.add(contactInfo);
+            }
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+        return addressBookList;
+
+    }
+}
--- src/com/bridgelabz/JDBCDatabaseAddressBookProblem.java
+++
@@ -1,9 +0,0 @@
-package com.bridgelabz;
-
-public class JDBCDatabaseAddressBookProblem {
-
-	public static void main(String[] args) {
-System.out.println("Welcome to JDBC-Database-AddressBook-Problem");
-	}
-
-}
